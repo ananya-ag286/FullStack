9849c13280280e5d28a6c0021492e0ed448e39cb
ananya-ag286 <ananyaagrawal286@gmail.com>
2026-07-31 09:28:24 +0530

    2 new files added

---
+++ test.js
@@ -0,0 +1,13 @@
+import {sum, add} from './demo4.js';
+
+console.log(sum(1));
+console.log(sum(1, 2));
+console.log(sum(1, 2, 3));
+console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4, 5));
+
+console.log(add(1));
+console.log(add(1, 2));
+console.log(add(1, 2, 3));
+console.log(add(1, 2, 3, 4));
+console.log(add(1, 2, 3, 4, 5));
